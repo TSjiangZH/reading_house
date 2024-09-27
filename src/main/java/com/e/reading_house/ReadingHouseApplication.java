fd8ed23c025a163d19334b1dce5f73a4c512a454
TSjiangZH <1362365932@qq.com>
2024-09-27 21:32:44 +0800

    第一次提交

---
+++ src/main/java/com/e/reading_house/ReadingHouseApplication.java
@@ -0,0 +1,13 @@
+package com.e.reading_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingHouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/e/reading_house/ReadingHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e.reading_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
